28eef9671b1f13601f27ec49b8b28b56ed6c4bea
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2015-09-28 15:01:01 +1000

    web: comment artoolkit.three.js

--- web/artk_demos/js/artoolkit.three.js
+++ web/artk_demos/js/artoolkit.three.js
@@ -1,9 +1,45 @@
 /* THREE.js ARToolKit integration */
 
+/**
+	Set this matrix's elements to the given column-major matrix array.
+
+	@param {Float32Array} m - The array to copy
+*/
 THREE.Matrix4.prototype.setFromArray = function(m) {
 	return this.elements.set(m);
 };
 
+/**
+	Helper for setting up a Three.js AR scene using the device camera as input.
+	Pass in the maximum dimensions of the video you want to process and onSuccess and onError callbacks.
+
+	On a successful initialization, the onSuccess callback is called with an ThreeARScene object.
+	The ThreeARScene object contains two THREE.js scenes (one for the video image and other for the 3D scene)
+	and a couple of helper functions for doing video frame processing and AR rendering.
+
+	Here's the structure of the ThreeARScene object:
+	{
+		scene: THREE.Scene, // The 3D scene. Put your AR objects here.
+		camera: THREE.Camera, // The 3D scene camera.
+
+		video: HTMLVideoElement, // The userMedia video element.
+
+		videoScene: THREE.Scene, // The userMedia video image scene. Shows the video feed.
+		videoCamera: THREE.Camera, // Camera for the userMedia video scene.
+
+		process: function(), // Process the current video frame and update the markers in the scene.
+		renderOn: function( THREE.WebGLRenderer ) // Render the AR scene and video background on the given Three.js renderer.
+	}
+
+	You should use the arScene.video.videoWidth and arScene.video.videoHeight to set the width and height of your renderer.
+	
+	In your frame loop, use arScene.process() and arScene.renderOn(renderer) to do frame processing and 3D rendering, respectively.
+	
+	@param {number} width - The maximum width of the userMedia video to request.
+	@param {number} height - The maximum height of the userMedia video to request.
+	@param {function} onSuccess - Called on successful initialization with an ThreeARScene object.
+	@param {function} onError - Called if the initialization fails with the error encountered.
+*/
 artoolkit.getUserMediaThreeScene = function(width, height, onSuccess, onError) {
 	artoolkit.init('/builds', '/bin/Data/camera_para.dat');
 	if (!onError) {
@@ -116,6 +152,11 @@ artoolkit.createThreeScene = function(video) {
 	};
 };
 
+/**
+	Overrides the artoolkit.onGetMarker method to keep track of Three.js markers.
+
+	@param {Object} marker - The marker object received from ARToolKitJS.cpp
+*/
 artoolkit.onGetMarker = function(marker) {
 	var obj = this.markers[marker.id];
 	if (obj) {
@@ -124,21 +165,42 @@ artoolkit.onGetMarker = function(marker) {
 	}
 };
 
+/**
+	Index of Three.js markers, maps markerID -> THREE.Object3D.
+*/
 artoolkit.markers = {};
 
-artoolkit.loadMarker = function(url, onSuccess, onError) {
-	if (!onError) {
-		onError = function(err) {
-			console.log("ERROR: artoolkit.loadMarker");
-			console.log(err);
-		};
-	}
-	artoolkit.addMarker('/bin/Data/patt.hiro', onSuccess, onError);
-}
+/**
+	Loads a marker from the given URL and calls the onSuccess callback with the UID of the marker.
 
-artoolkit.createThreeMarker = function(marker) {
+	artoolkit.loadMarker(markerURL, onSuccess, onError);
+
+	Synonym for artoolkit.addMarker.
+
+	@param {string} markerURL - The URL of the marker pattern file to load.
+	@param {function} onSuccess - The success callback. Called with the id of the loaded marker on a successful load.
+	@param {function} onError - The error callback. Called with the encountered error if the load fails.
+*/
+artoolkit.loadMarker = artoolkit.addMarker;
+
+/**
+	Creates a Three.js marker Object3D for the given marker UID. 
+	The marker Object3D tracks the marker pattern when it's detected in the video.
+
+	Use this after a successful artoolkit.loadMarker call:
+
+	artoolkit.loadMarker('/bin/Data/patt.hiro', function(markerUID) {
+		var markerRoot = artoolkit.createThreeMarker(markerUID);
+		markerRoot.add(myFancyHiroModel);
+		arScene.scene.add(markerRoot);
+	});
+
+	@param {number} markerUID - The UID of the marker to track.
+	@return {THREE.Object3D} Three.Object3D that tracks the given marker.
+*/
+artoolkit.createThreeMarker = function(markerUID) {
 	var obj = new THREE.Object3D();
 	obj.matrixAutoUpdate = false;
-	this.markers[marker] = obj;
+	this.markers[markerUID] = obj;
 	return obj;
 };
