6c66a406e6a637ecae35560bcc05c0301680f15b
Joshua Koo <zz85nus@gmail.com>
2015-09-28 11:29:41 +0800

    Fix "invalid operands to binary expression" compilation error

--- emscripten/ARToolKitJS.cpp
+++ emscripten/ARToolKitJS.cpp
@@ -157,7 +157,7 @@ extern "C" {
 	}
 
 	static int setupMarker(const char *patt_name, int *patt_id, ARHandle *arhandle, ARPattHandle **pattHandle_p) {
-		if (*gARPattHandle != NULL) {
+		if (gARPattHandle != NULL) {
 			ARLOGe("setupMarker(): arPattCreateHandle already created.\n");
 		} else if ((*pattHandle_p = arPattCreateHandle()) == NULL) {
 			ARLOGe("setupMarker(): Error: arPattCreateHandle.\n");
