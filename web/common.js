514a3830bd46b74533a6c6275d2092469632ba79
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2015-10-13 18:10:11 +0800

    common.js: first pass at ARController JS object

--- web/common.js
+++ web/common.js
@@ -1,6 +1,144 @@
 (function() {
 	'use strict'
 
+
+	var ARController = function(width, height, camera) {
+		var id;
+		var w = width, h = height;
+
+		if (typeof width !== 'number') {
+			var image = width;
+			w = image.videoWidth || image.width;
+			h = image.videoHeight || image.height;
+		}
+
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = w;
+		this.canvas.height = h;
+		this.ctx = this.canvas.getContext('2d');
+
+		this.addEventListeners();
+
+		this.id = artoolkit.setup(w, h, camera);
+	};
+
+	ARController.prototype.addEventListeners = function() {
+
+		var self = this;
+
+		artoolkit.addEventListener('markerNum', function(ev) {
+			if (ev.target === self.id) {
+				console.log('ARController '+self.id+' got marker num', ev.data);
+			}
+		});
+
+		artoolkit.addEventListener('frameMalloc', function(ev) {
+			if (ev.target === self.id || self.id === undefined) {
+
+				console.log('ARController '+ev.target+' got frame malloc', ev.data);
+
+				var params = ev.data;
+				self.framepointer = params.framepointer;
+				self.framesize = params.framesize;
+
+				self.dataHeap = new Uint8Array(Module.HEAPU8.buffer, self.framepointer, self.framesize);
+
+				self.camera_mat = new Float64Array(Module.HEAPU8.buffer, params.camera, 16);
+				self.transform_mat = new Float64Array(Module.HEAPU8.buffer, params.modelView, 16);
+			}
+		});
+
+		artoolkit.addEventListener('getMarker', function(ev) {
+			if (ev.target === self.id) {
+				console.log('ARController '+self.id+' got marker', ev.data, ev.index);
+			}
+		});
+
+		artoolkit.addEventListener('getMultiMarker', function(ev) {
+			if (ev.target === self.id) {
+				console.log('getMultiMarker', ev.data);
+			}
+		});
+
+		artoolkit.addEventListener('getMultiMarkerSub', function(ev) {
+			if (ev.target === self.id) {
+			}
+		});
+
+	};
+
+	ARController.prototype.debugSetup = function() {
+		artoolkit.debugSetup(this.id);
+	};
+
+	ARController.prototype.process = function(image) {
+		this.ctx.drawImage(image, 0, 0, w, h); // draw video
+
+		var imageData = this.ctx.getImageData(0, 0, w, h);
+		var data = imageData.data;
+
+		if (this.dataHeap) {
+			this.dataHeap.set( new Uint8Array(data.buffer) );
+
+			artoolkit.process(this.id);
+
+			this.debugDraw();
+		}
+	};
+
+	ARController.prototype.debugSetup = function() {
+		document.body.appendChild(this.canvas)
+		this.bwpointer = artoolkit.setDebugMode(this.id, 1);
+	};
+
+	ARController.prototype.debugDraw = function() {
+		var debugBuffer = new Uint8ClampedArray(Module.HEAPU8.buffer, this.bwpointer, this.framesize);
+		var id = new ImageData(debugBuffer, w, h)
+		this.ctx.putImageData(id, 0, 0)
+
+		if (!marker) return;
+
+		for (var i=0; i<detected_markers.length; i++) {
+			this.debugMarker(detected_markers[i]);
+		}
+	};
+
+	ARController.prototype.debugMarker = function(marker) {
+		var vertex, pos;
+		vertex = marker.vertex;
+		var ctx = this.ctx;
+		ctx.strokeStyle = 'red';
+
+		ctx.beginPath()
+		ctx.moveTo(vertex[0][0], vertex[0][1])
+		ctx.lineTo(vertex[1][0], vertex[1][1])
+		ctx.stroke();
+
+		ctx.beginPath()
+		ctx.moveTo(vertex[2][0], vertex[2][1])
+		ctx.lineTo(vertex[3][0], vertex[3][1])
+		ctx.stroke()
+
+		ctx.strokeStyle = 'green';
+		ctx.beginPath()
+		ctx.lineTo(vertex[1][0], vertex[1][1])
+		ctx.lineTo(vertex[2][0], vertex[2][1])
+		ctx.stroke();
+
+		ctx.beginPath()
+		ctx.moveTo(vertex[3][0], vertex[3][1])
+		ctx.lineTo(vertex[0][0], vertex[0][1])
+		ctx.stroke();
+
+		pos = marker.pos
+		ctx.beginPath()
+		ctx.arc(pos[0], pos[1], 8, 0, Math.PI * 2)
+		ctx.fillStyle = 'red'
+		ctx.fill()
+	}
+
+
+
 	var path = '';
 
 	var EMSCRIPTEN_FILE = 'artoolkit.js';
@@ -23,6 +161,33 @@
 		setup: setup,
 		process: process,
 
+		listeners: {},
+
+		addEventListener: function(name, callback) {
+			if (!this.listeners[name]) {
+				this.listeners[name] = [];
+			}
+			this.listeners[name].push(callback);
+		},
+
+		removeEventListener: function(name, callback) {
+			if (this.listeners[name]) {
+				var index = this.listeners[name].indexOf(callback);
+				if (index > -1) {
+					this.listeners[name].splice(index, 1);
+				}
+			}
+		},
+
+		dispatchEvent: function(event) {
+			var listeners = this.listeners[event.name];
+			if (listeners) {
+				for (var i=0; i<listeners.length; i++) {
+					listeners[i](event);
+				}
+			}
+		},
+
 		addMarker: addMarker,
 		addMultiMarker: addMultiMarker,
 
@@ -30,7 +195,10 @@
 		onMarkerNum: onMarkerNum,
 		_onGetMarker: onGetMarker,
 
-		debugSetup: debugSetup,
+		onGetMultiMarker: onGetMultiMarker,
+		onGetMultiMarkerSub: onGetMultiMarkerSub,
+
+		setDebugMode: setDebugMode,
 
 		getDetectedMarkers: function() {
 			return detected_markers;
@@ -142,28 +310,41 @@
 	}
 
 	function onFrameMalloc(id, params) {
-		framepointer = params.framepointer;
-		framesize = params.framesize;
+		this.dispatchEvent({name: 'frameMalloc', target: id, data: params});
 
-		camera_mat = new Float64Array(Module.HEAPU8.buffer, params.camera, 16);
-		transform_mat = new Float64Array(Module.HEAPU8.buffer, params.modelView, 16);
+		// framepointer = params.framepointer;
+		// framesize = params.framesize;
+
+		// camera_mat = new Float64Array(Module.HEAPU8.buffer, params.camera, 16);
+		// transform_mat = new Float64Array(Module.HEAPU8.buffer, params.modelView, 16);
 
 		// console.log('onMalloc', params);
 		// console.log('matrices', camera_mat, transform_mat);
 	}
 
 	function onMarkerNum(id, number) {
+		this.dispatchEvent({name: 'markerNum', target: id, data: number});
+
 		detected_markers = new Array(number);
 		// console.log('Detected', number);
 	}
 
 	function onGetMarker(object, i, id) {
-		console.log(arguments);
+		this.dispatchEvent({name: 'getMarker', target: id, index: i, data: object});
+
 		marker = object;
 		detected_markers[i] = marker;
 
 		if (artoolkit.onGetMarker) artoolkit.onGetMarker(arID, object, i);
 		// console.log(marker.id, marker.idMatrix, marker.cf);
+	}
+
+	function onGetMultiMarker(id, object) {
+		this.dispatchEvent({name: 'getMultiMarker', target: id, data: object});
+	}
+
+	function onGetMultiMarkerSub(id, multiId, subMarker, subMarkerId) {
+		this.dispatchEvent({name: 'getMultiMarkerSub', target: id, data: {multiMarkerId: multiId, markerId: subMarkerId, marker: subMarker}});
 	}
 
 	function setup(_w, _h) {
@@ -177,107 +358,37 @@
 		artoolkit.setProjectionNearPlane(arID, 0.1)
 		artoolkit.setProjectionFarPlane(arID, 1000);
 
-		// setup canvas
-		canvas = document.createElement('canvas');
-		canvas.width = w;
-		canvas.height = h;
-		ctx = canvas.getContext('2d')
-
 		return arID;
 	}
 
 	var marker_count = 0;
-	function addMarker(url, callback) {
+	function addMarker(arId, url, callback) {
 		var filename = '/marker_' + marker_count++;
 		ajax(url, filename, function() {
-			var id = Module._addMarker(arID, filename);
+			var id = Module._addMarker(arId, filename);
 			if (callback) callback(id);
 		});
 	}
 
 	var multi_marker_count = 0;
-	function addMultiMarker(url, callback) {
+	function addMultiMarker(arId, url, callback) {
 		var filename = '/multi_marker_' + multi_marker_count++;
 		ajax(url, filename, function() {
-			var markerID = Module._addMultiMarker(arID, filename);
-			var markerNum = Module._getMultiMarkerNum(arID, markerID);
+			var markerID = Module._addMultiMarker(arId, filename);
+			var markerNum = Module._getMultiMarkerNum(arId, markerID);
 			if (callback) callback(markerID, markerNum);
 		});
 	}
 
-	var bwpointer;
-
-	function debugSetup() {
-		document.body.appendChild(canvas)
-		bwpointer = _setDebugMode(arID, 1);
+	function setDebugMode(arId, mode) {
+		return _setDebugMode(arId, mode);
 	}
 
-	function debugDraw() {
-		var debugBuffer = new Uint8ClampedArray(Module.HEAPU8.buffer, bwpointer, framesize);
-		var id = new ImageData(debugBuffer, w, h)
-		ctx.putImageData(id, 0, 0)
-
-		if (!marker) return;
-
-		detected_markers.forEach(debugMarker);
-	}
-
-	function debugMarker(marker) {
-		var vertex, pos;
-		vertex = marker.vertex;
-		ctx.strokeStyle = 'red';
-
-		ctx.beginPath()
-		ctx.moveTo(vertex[0][0], vertex[0][1])
-		ctx.lineTo(vertex[1][0], vertex[1][1])
-		ctx.stroke();
-
-		ctx.beginPath()
-		ctx.moveTo(vertex[2][0], vertex[2][1])
-		ctx.lineTo(vertex[3][0], vertex[3][1])
-		ctx.stroke()
-
-		ctx.strokeStyle = 'green';
-		ctx.beginPath()
-		ctx.lineTo(vertex[1][0], vertex[1][1])
-		ctx.lineTo(vertex[2][0], vertex[2][1])
-		ctx.stroke();
-
-		ctx.beginPath()
-		ctx.moveTo(vertex[3][0], vertex[3][1])
-		ctx.lineTo(vertex[0][0], vertex[0][1])
-		ctx.stroke();
-
-		pos = marker.pos
-		ctx.beginPath()
-		ctx.arc(pos[0], pos[1], 8, 0, Math.PI * 2)
-		ctx.fillStyle = 'red'
-		ctx.fill()
-	}
 
 	// transfer image
 
-	function process(target) {
-
-		// console.time('draw');
-		ctx.drawImage(target, 0, 0, w, h); // draw video
-		// console.timeEnd('draw');
-
-		// console.time('getImage');
-		image = ctx.getImageData(0, 0, w, h);
-		data = image.data;
-		// console.time('getImage');
-
-		// console.time('transferImage');
-		var dataHeap = new Uint8Array(Module.HEAPU8.buffer, framepointer, framesize);
-		dataHeap.set( new Uint8Array(data.buffer) );
-		console.timeEnd('transferImage');
-
-		// console.time('process')
-		_process(arID);
-		// console.timeEnd('process')
-
-		debugDraw();
+	function process(arId) {
+		_process(arId);
 	}
 
 	// Eg.
@@ -317,5 +428,6 @@
 
 	window.Module = Module;
 	window.artoolkit = artoolkit;
+	window.ARController = ARController;
 
 })();
